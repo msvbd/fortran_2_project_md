12ce6cdadbcfcd051a7c3eec59f660cfa2538048
RetOI3 <166544119+RetOI3@users.noreply.github.com>
2024-04-09 19:49:41 +0200

    Update Atom.f90

--- modules/Atom.f90
+++ modules/Atom.f90
@@ -1,6 +1,10 @@
 module Atom
 implicit none
-
-
-
-end module Atom
+    private
+            
+    public:: Atom_obj 
+        
+    type Atom_obj  
+        real, allocatable:: Radius(:), Velocity(:), Mass(:), Force(:)
+    end type Atom_obj 
+end module
